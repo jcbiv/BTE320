885c3d1363d5a9b790ea00a568e25ec3d3ab10a3
jcbiv <133532349+jcbiv@users.noreply.github.com>
2024-02-28 13:58:37 -0500

    Add files via upload

---
+++ MoreStrings.py
@@ -0,0 +1,11 @@
+L = []
+
+print("Enter numbers and let's make a list!! \n")
+
+number = 2
+
+while number != 0:
+
+    number = int(input("Enter a value: "))
+    L.append(number)
+    print(f"Your list = {L}")
