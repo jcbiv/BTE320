c68306d0d817b3273053069c298d63a04ad3137c
jcbiv <133532349+jcbiv@users.noreply.github.com>
2024-03-04 14:02:11 -0500

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+
+
+months = {}
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+for i in range(0,12):
+
+    months[numbers[i]] = names[i]
+
+print(months)
